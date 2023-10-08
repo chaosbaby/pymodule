a763bad8bd45637533baccd9edf1e38448877c04
chaos <50092526@qq.com>
2023-10-09 03:47:10 +0800

    apply nest_asyncio, and make callback return only a info dic

--- reqaio/reqaio/aio.py
+++ reqaio/reqaio/aio.py
@@ -1,15 +1,21 @@
 import aiohttp
 import asyncio
 from fake_useragent import UserAgent
+from libext.Dict import Dict
+import nest_asyncio
+nest_asyncio.apply()
 
 async def fetch(session, url, callback):
     
     async with session.get(url) as response:
         text = await response.text(errors="ignore")
-        callback(url, text)
+        info = Dict()
+        info.url = url
+        info.text = text
+        await callback(info)
 
 
-async def send_requests(urls, callback,headers={},cookies={}):
+async def requests(urls, callback,headers={},cookies={}):
     if not headers:
         headers={"User-Agent": UserAgent().random} 
     async with aiohttp.ClientSession(headers=headers,cookies=cookies) as session:
@@ -17,10 +23,14 @@ async def send_requests(urls, callback,headers={},cookies={}):
         for url in urls:
             task = asyncio.ensure_future(fetch(session, url, callback))
             tasks.append(task)
-        await asyncio.gather(*tasks, return_exceptions=True)
+        await asyncio.gather(*tasks, return_exceptions=False)
+        # await asyncio.gather(*tasks)
 
 def run(urls, callback,headers=None,cookies=None):
-    asyncio.run(send_requests(urls, callback,headers,cookies))
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(requests(
+    #     urls, callback,headers,cookies))
+    asyncio.run(requests(urls, callback,headers,cookies))
 
     
 def test():
