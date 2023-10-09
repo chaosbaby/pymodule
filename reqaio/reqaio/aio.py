222b6aab356841af5fc263109168edbfe63aeae8
chaos <50092526@qq.com>
2023-10-09 16:01:29 +0800

    aio api with param to enable many diff session request

--- reqaio/reqaio/aio.py
+++ reqaio/reqaio/aio.py
@@ -5,13 +5,14 @@ from libext.Dict import Dict
 import nest_asyncio
 nest_asyncio.apply()
 
-async def fetch(session, url, callback):
+async def fetch(session, url, callback,extra=None):
     
     async with session.get(url) as response:
         text = await response.text(errors="ignore")
         info = Dict()
         info.url = url
         info.text = text
+        info.extra = extra
         await callback(info)
 
 
@@ -24,13 +25,32 @@ async def requests(urls, callback,headers={},cookies={}):
             task = asyncio.ensure_future(fetch(session, url, callback))
             tasks.append(task)
         await asyncio.gather(*tasks, return_exceptions=False)
-        # await asyncio.gather(*tasks)
+
+async def request(url,callback=None,headers={},cookies={},extra=None):  # <6>
+    async with aiohttp.ClientSession(cookies=cookies, headers=headers) as session:
+        await fetch(session, url, callback)
+
+async def params_requests(params,callback):
+    tasks = []
+    for param in params:
+        url = param.get('url')
+        headers = param.get('headers')
+        if not headers:
+            headers={"User-Agent": UserAgent().random} 
+        cookies = param.get('cookies')        
+        task = asyncio.ensure_future(request(url, callback,headers,cookies,extra=param))
+        tasks.append(task)
+    await asyncio.gather(*tasks, return_exceptions=False)
 
 def run(urls, callback,headers=None,cookies=None):
     # loop = asyncio.get_event_loop()
     # loop.run_until_complete(requests(
     #     urls, callback,headers,cookies))
     asyncio.run(requests(urls, callback,headers,cookies))
+
+def params_run(params,callback):
+    asyncio.run(params_requests(params,callback))
+
 
     
 def test():
